5305491de2e0a30d58f56120e39defec4b8b4ca5
Johannes A <john.rans@web.de>
2022-06-02 14:27:52 +0200

    Randombot buxfix range outside of board

--- Player/Bot/RandomBot.cpp
+++ Player/Bot/RandomBot.cpp
@@ -4,6 +4,6 @@
 unsigned int RandomBot::chooseColumn() {
     std::mt19937 rng;
     rng.seed(std::random_device()());
-    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, 6);
+    std::uniform_int_distribution<std::mt19937::result_type> dist6(0, Board::FIELD_WIDTH-1);
     return dist6(rng);
 }
